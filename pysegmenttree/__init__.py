5c4797087d6ee600f0b24a5cdf1bd21629331c69
Viacheslav Greshilov <slovaricheg@gmail.com>
2021-11-27 22:47:40 +0200

    Bump version to 0.2.0

--- pysegmenttree/__init__.py
+++ pysegmenttree/__init__.py
@@ -1,5 +1,5 @@
 from ._abc import QueryFunction
 from ._segmenttree import PySegmentTree, stree
 
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 __all__ = ["PySegmentTree", "QueryFunction", "stree"]
