2ca987f0a03da6777f779ec0d15052b512ef3b1c
Viacheslav Greshilov <slovaricheg@gmail.com>
2021-11-28 02:16:35 +0200

    Fix Py_UNREACHABLE missing in 3.6

--- pysegmenttree/_extensions/common.h
+++ pysegmenttree/_extensions/common.h
@@ -1,6 +1,11 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
+/* Py_UNREACHABLE was introduced only in 3.7 */
+#if !defined Py_UNREACHABLE
+#define Py_UNREACHABLE() Py_FatalError("Unreachable C code path reached")
+#endif
+
 enum QueryFunc {
     Sum = 1,
     Min,
